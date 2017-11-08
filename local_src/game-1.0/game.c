bdebf907ab248e8c6361f4e9638322e8002f0d0b
smcarlse <signe_c_97@hotmail.com>
2017-11-08 16:47:25 +0100

    star

--- local_src/game-1.0/game.c
+++ local_src/game-1.0/game.c
@@ -20,9 +20,52 @@ size_t screensize = (320 * 240 * 16) / 8;
 short *map;
 int oflags;
 int e;
+int x = 0;
+int star = 0; 
 
 void button_handler(int signal) {
 	printf("dette er et signal \n");
+
+	int numberOfBytesRead __attribute__ ((unused));
+        unsigned int buttonValue;
+        numberOfBytesRead = read(gamepadDriver, &buttonValue, 4);
+	printf("buttonValue fra game: %d  number of bytes read: %d \n",numberOfBytesRead, buttonValue);
+		// setup which part of the framebuffer that is to be refreshed 
+	// for performance reasons, use as small rectangle as possible 
+	struct fb_copyarea rect;
+	rect.dx = 0;
+	rect.dy = 235;
+	rect.width = 320;
+	rect.height = 1;
+
+
+	int i;
+	int j;
+
+	if(buttonValue == 4 ){
+		for (i = 320*235; i < (320*236); i++) {
+			map[i] = 0; 
+		}
+		for (i = 320*235+x; i < (320*235)+50+x; i++) {
+			map[i] = 65535; 
+		}
+		if(x < 320-60){
+			x += 20;
+		}
+	}else if(buttonValue == 1 ){
+		for (i = 320*235; i < (320*236); i++) {
+			map[i] = 0; 
+		}
+		for (i = 320*235+x; i < (320*235)+50+x; i++) {
+			map[i] = 65535; 
+		}
+		if(x != 0){
+			x -= 20;
+		}
+	}
+	// command driver to update display
+	ioctl(fbfd, 0x4680, &rect);
+	
 	return;
 }
 
@@ -36,33 +79,15 @@ int main(int argc, char *argv[])
 	
 	map = (short*) mmap(NULL, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
 	printf("Mapping %p \n", map);
-
-	// setup which part of the framebuffer that is to be refreshed 
-	// for performance reasons, use as small rectangle as possible 
-	struct fb_copyarea rect;
-	rect.dx = 0;
-	rect.dy = 0;
-	rect.width = 320;
-	rect.height = 240;
-	
-	int i;
-	int j;
-	for (i = 320*235; i < (320*235)+50; i++) {
-		map[i] = 8; 
-	}	
-
-	// command driver to update display
-	ioctl(fbfd, 0x4680, &rect);
+		
 
 	printf("Hello World, I'm game!\n");
 	gamepadDriver = open(GAMEPAD, O_RDONLY);
 	if(errno == EACCES) printf("EACCES\n");
 	printf("error 1: %s \n", strerror(errno));
 	printf("gamepaddriver: %d \n", gamepadDriver);
-        int numberOfBytesRead __attribute__ ((unused));
-        unsigned int buttonValue;
-        numberOfBytesRead = read(gamepadDriver, &buttonValue, 4);
-	printf("buttonValue fra game: %d  number of bytes read: %d \n",numberOfBytesRead, buttonValue);
+
+	
 	printf("error 2: %d \n", errno);
 
 		
@@ -73,8 +98,44 @@ int main(int argc, char *argv[])
 	e = fcntl(gamepadDriver, F_SETFL, oflags | FASYNC);
 	printf("fcntl %d \n", e);
 	printf("error 1: %s \n", strerror(errno));
+	
+	struct fb_copyarea rect;
+	rect.dx = 0;
+	rect.dy = 0;
+	rect.width = 320;
+	rect.height = 240;
 
-	while(1);
+	int i;
+
+	for (i = 0; i < (320*240); i++) {
+		map[i] = 0; 
+	}
+	// command driver to update display
+	ioctl(fbfd, 0x4680, &rect);
+	
+	star = rand()%320;
+	printf("hei dette er et random tall: %d \n", star);
+	rect.dx = star;
+	rect.dy = 0;
+	rect.width = 1;
+	rect.height = 240;
+	int count = 0;
+	
+	while(1)
+	{
+		count += 1;
+		if (count %30000 == 0){
+		int k;
+		for (k = star; k<240*320; k+=320){
+			map[k]=0;
+		}
+		map[star] = 65355;
+		star += 320;
+
+		ioctl(fbfd, 0x4680, &rect);
+		}
+			
+	}
 	exit(EXIT_SUCCESS);
 }
 
